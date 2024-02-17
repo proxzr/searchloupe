c8210cd8e5cadc60f684de79ddc1afa40c207c23
proX <152530696+proxzr@users.noreply.github.com>
2024-02-17 12:26:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import webbrowser, argparse
+import webbrowser, argparse, sys
 
 def cli():
     parse = argparse.ArgumentParser()
@@ -59,16 +59,9 @@ def cli():
         help='use tiktok'
     )
 
-    parse.add_argument(
-        '-s', '--search',
-        nargs='?',
-        type=str,
-        default=None,
-        help='your search'
-    )
+    parse.add_argument('search')
 
     args = parse.parse_args()
-
     searchdot = args.search
 
     return args, searchdot
@@ -77,9 +70,27 @@ def launcher_search_engine():
 
     args, searchdot = cli()
 
+    if len(sys.argv) == 2 and args.search:
+        try:
+            print("✔️ Search with all search engines")
+            webbrowser.open("https://www.google.com/search?q=" + searchdot)
+            webbrowser.open("https://www.bing.com/search?form=&q=" + searchdot)
+            webbrowser.open("https://search.yahoo.com/search?p=" + searchdot + "&ei=UTF-8&fp=1")
+            webbrowser.open("https://duckduckgo.com/?t=h_&q=" + searchdot + "&ia=web")
+            webbrowser.open("https://yandex.com/search/?text=" + searchdot + "&search_source=yacom_desktop_common")
+            webbrowser.open("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd=" + searchdot + "&rn=&fenlei=256&rqlang=en")
+            webbrowser.open("https://www.ecosia.org/search?method=index&q=" + searchdot)
+            webbrowser.open("https://www.qwant.com/?l=enr&q=" + searchdot + "&t=web")
+            webbrowser.open("https://twitter.com/search?q=" + searchdot + "&src=typed_query&f=top")
+            webbrowser.open("https://www.facebook.com/search/top/?q=" + searchdot)
+            webbrowser.open("https://www.tiktok.com/search?q=" + searchdot)
+        except:
+            print("❌ Error on search with all search engines")
+
+
     if args.google:
         try:
-            webbrowser.open("https://www.google.com/search?q=" + searchdot +"&sca_esv=586707143&source=hp&ei=zOdoZci8IKKrkdUPzqCXyAg&iflsig=AO6bgOgAAAAAZWj13BSrYg41mAM-nxwxHFJTRco1YQZ7&ved=0ahUKEwjI0uLXv-yCAxWiVaQEHU7QBYkQ4dUDCAo&uact=5&oq=test&gs_lp=Egdnd3Mtd2l6IgR0ZXN0MhAQABgDGI8BGOUCGOoCGIwDMhAQLhgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQLhgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDMhAQABgDGI8BGOUCGOoCGIwDSJAJUIcFWOMGcAF4AJABAJgBAKABAKoBALgBA8gBAPgBAagCCg&sclient=gws-wiz")
+            webbrowser.open("https://www.google.com/search?q=" + searchdot)
             print("✔️ Google")
 
         except Exception:
@@ -88,7 +99,7 @@ def launcher_search_engine():
 
     if args.bing:
         try:
-            webbrowser.open("https://www.bing.com/search?form=&q=" + searchdot + "&form=QBLH&sp=-1&lq=1&pq=&sc=0-0&qs=n&sk=&cvid=74B6895DB48342D3B8892A5AFE5C5881&ghsh=0&ghacc=0&ghpl=")
+            webbrowser.open("https://www.bing.com/search?form=&q=" + searchdot)
             print("✔️ Bing")
 
         except Exception:
@@ -96,7 +107,7 @@ def launcher_search_engine():
     
     if args.yahoo:
         try:
-            webbrowser.open("https://search.yahoo.com/search?p=" + searchdot + "&fr=yfp-t&fr2=p%3Afp%2Cm%3Asb&ei=UTF-8&fp=1")
+            webbrowser.open("https://search.yahoo.com/search?p=" + searchdot + "&ei=UTF-8&fp=1")
             print("✔️ Yahoo")
 
         except Exception:
@@ -112,7 +123,7 @@ def launcher_search_engine():
 
     if args.yandex:
         try:
-            webbrowser.open("https://yandex.com/search/?text=" + searchdot + "&lr=21081&search_source=yacom_desktop_common")
+            webbrowser.open("https://yandex.com/search/?text=" + searchdot + "&search_source=yacom_desktop_common")
             print("✔️ Yandex")
 
         except Exception:
@@ -120,7 +131,7 @@ def launcher_search_engine():
 
     if args.baidu:
         try:
-            webbrowser.open("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd=" + searchdot + "&rn=&fenlei=256&oq=&rsv_pq=0x8f99c62900020204&rsv_t=dd74Oy0Y3JEDSDl8GFz3pNCzyve%2B2z7C1BdKNCH%2BiyWtsQ6oncphqvblpuVh&rqlang=en")
+            webbrowser.open("https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&ch=&tn=baidu&bar=&wd=" + searchdot + "&rn=&fenlei=256&rqlang=en")
             print("✔️ Baidu")
 
         except Exception:
@@ -165,7 +176,7 @@ def launcher_search_engine():
         
         except Exception:
             print("❌ Tiktok")
-        
+
 
 def main(): 
     print("""
@@ -180,9 +191,7 @@ def main():
     Made by Prox --- https://twitter.com/OsintTheWorld
     """)
 
-    
     launcher_search_engine()
-
 
 if __name__ == "__main__":
     main()
